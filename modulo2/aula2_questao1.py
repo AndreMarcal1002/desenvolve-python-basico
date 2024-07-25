da73d28d94241201c4e27871742c2a7d16ea96b2
André Marçal <andre.marcal@pdbomdespacho.com.br>
2024-07-24 22:46:09 -0300

    Add files via upload

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,11 @@
+# Armazene o valor 5 em uma variável e o valor 2 em outra variável
+# Imprima o tipo de dados dessas duas variáveis
+# Divida a primeira variável (com o valor 5) pela segunda variável (com o valor 2) 
+# e armazene o resultado em uma terceira variável
+# Imprima a terceira variável e também o seu tipo 
+num1=5 
+num2=2 
+print("num1 é do tipo: ", type(num1))
+print("num2 é do tipo: ", type(num2))
+num3=num1/num2 
+print("num3", num3, " é do tipo: ", type(num3))
---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,14 @@
+# 2) Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já 
+# conhecemos de forma geral. 
+# Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, 
+# e o valor 3.5 numa terceira variável.
+# Some os valores da segunda e terceira variável e armazene em outra variável.
+# Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+texto1="o resultado é" 
+num1=10 
+num2=3.5 
+num3=num1+num2 
+print(texto1, "texto1 e do tipo: ", type(texto1))
+print(num1, "num1 é do tipo: ", type(num1))
+print(num2, "num2 é do tipo: ", type(num2))
+print(num3,"num3 é do tipo: ", type(num3))
